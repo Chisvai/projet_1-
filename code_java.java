36fe82693264a4980cee47749d7a234577d30026
chi_svai <chi_svai@hotmail.fr>
2017-11-28 20:06:05 +0100

    ajout de numero de téléphone

--- code_java.java
+++ code_java.java
@@ -9,7 +9,7 @@ public class Chi1 {
 		System.out.println(" My name is :");
 		System.out.print("Chihab :)");
 		System.out.print("ma date de naissance : 25/7/2015");
-
+		System.out.print("numero de telephone: 5698741600");
 	}
 
 }
