14dab536a3699d8623911bc3f40dfd9e6c24ed82
chi_svai <chi_svai@hotmail.fr>
2017-11-28 19:59:04 +0100

    ajout d'une date de naissance a la personne

--- code_java.java
+++ code_java.java
@@ -8,7 +8,7 @@ public class Chi1 {
 		System.out.print("hello wolrd !\n");
 		System.out.println(" My name is :");
 		System.out.print("Chihab :)");
-		
+		System.out.print("ma date de naissance : 25/7/2015");
 
 	}
 
