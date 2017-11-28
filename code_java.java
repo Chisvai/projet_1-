17b2fc6de381a6f2bccb371cf4504252784272a8
chi_svai <chi_svai@hotmail.fr>
2017-11-28 19:53:37 +0100

    création d'un nouveau fichier java

---
+++ code_java.java
@@ -0,0 +1,15 @@
+
+package projet1;
+
+public class Chi1 {
+
+	/*byte code */
+	public static void main(String[] args) {
+		System.out.print("hello wolrd !\n");
+		System.out.println(" My name is :");
+		System.out.print("Chihab :)");
+		
+
+	}
+
+}
